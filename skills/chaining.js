8f61813ee3d962764c66458af030bbc6139ff300
jonchurch <me@jonchurch.com>
2017-03-14 20:33:15 -0400

    call convo.next() in ask callbacks

--- skills/chaining.js
+++ skills/chaining.js
@@ -2,15 +2,25 @@
 
 module.exports = function(controller) {
 
-controller.hears('?', 'message_received', function(bot, message){
+controller.hears('qq', 'message_received', function(bot, message){
     bot.startConversation(message, function(err, convo){
-        convo.say('Lets get to know each other a little bit!') 
-        convo.ask('Which is more offensive? Book burning or flag burning?', function(res, convo){
-            convo.ask('How often do you keep your promises?', function(res, convo){
-                convo.ask('Which is bigger? The Sun or the Earth?', function(res, convo){
-                    convo.say('Thank you, that is all for now â¤')})})
+            convo.say('Lets get to know each other a little bit!') 
+          
+            convo.ask('Which is more offensive? Book burning or flag burning?', function(res, convo){
+                  convo.next()
+                      
+                        convo.ask('How often do you keep your promises?', function(res, convo){
+                              convo.next()
+                                      
+                                        convo.ask('Which is bigger? The Sun or the Earth?', function(res, convo){
+                                                          
+                                                            convo.say('Thank you, that is all for now')
+                                              convo.next()
+                                                          
+                                                        })
+                                    })
+                    })
         })
-    })
 })
 
 }
